30f95ee799e816687b5d8f8633578a2c815b2a10
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-01-30 16:01:51 -0800

    added queue methods

--- clients/acme-widget.js
+++ clients/acme-widget.js
@@ -5,7 +5,14 @@ const cfps = io.connect('http://localhost:3001/cfps');
 
 cfps.emit('join', 'acme');
 
+cfps.on('queued', payload => {
+  payload.forEach(item => {
+    console.log(item);
+    cfps.emit('received', 'acme');
+  });
+});
+
 cfps.on('package-delivery', payload => {
   console.log(payload);
-  cfps.emit('received', payload);
+  cfps.emit('received', 'acme');
 });
--- clients/flowers-r-us.js
+++ clients/flowers-r-us.js
@@ -3,11 +3,16 @@
 const io = require('socket.io-client');
 const cfps = io.connect('http://localhost:3001/cfps');
 
-cfps.emit('join', 'flowers')
+cfps.emit('join', 'flowers');
 
-// cfps.on('received')
+cfps.on('queued', payload => {
+  payload.forEach(item => {
+    console.log(item);
+    cfps.emit('received', 'flowers');
+  });
+});
 
 cfps.on('package-delivery', payload => {
   console.log(payload);
-  cfps.emit('received', payload);
+  cfps.emit('received', 'flowers');
 });
